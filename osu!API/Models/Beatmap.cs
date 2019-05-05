3e54c64c83dffc1f2d278a0d715830be19b814c4
Albie Spriddell <albion@dragonfruit.ml>
2019-05-05 07:19:12 +0100

    Add project files.

---
+++ osu!API/Models/Beatmap.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DragonFruit.osu.API.Models
+{
+    public class Beatmap
+    {
+        [JsonProperty("approved")]
+        public osu.BeatmapStatus BeatmapStatus { get; set; }
+
+        [JsonProperty("approved_date")]
+        public DateTime ApprovedDate { get; set; }
+
+        [JsonProperty("last_update")]
+        public DateTime LastUpdated { get; set; }
+
+        [JsonProperty("artist")]
+        public string ArtistName { get; set; }
+
+        [JsonProperty("beatmap_id")]
+        public int BeatmapID { get; set; }
+
+        [JsonProperty("beatmapset_id")]
+        public int BeatmapSetID { get; set; }
+
+        [JsonProperty("bpm")]
+        public int BPM { get; set; }
+
+        [JsonProperty("creator")]
+        public string Mapper { get; set; }
+
+        [JsonProperty("difficultyrating")]
+        public float StarValue { get; set; }
+    }
+}
---
+++ osu!API/osu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DragonFruit.osu.API
+{
+    public class osu
+    {
+        public static readonly string endpointBase = "https://osu.ppy.sh/p/api/";
+        internal static string apiKey = "";
+
+        /// <summary>
+        /// Status codes that correspond to beatmap status
+        /// </summary>
+        public enum BeatmapStatus 
+        {
+            Loved = 4,
+            Qualified = 3,
+            Approved = 2,
+            Ranked = 1,
+            Pending = 0,
+            WIP = -1,
+            Graveyard = -2
+        }
+
+
+        /// <summary>
+        /// Sets the API key for the rest of the app to use
+        /// </summary>
+        /// <param name="APIKey"></param>
+        public static void SetAPIKey(string APIKey)
+        {
+            apiKey = APIKey;
+            return;
+        }
+    }
+}
